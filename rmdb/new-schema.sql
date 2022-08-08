0f5376b21825d7afd1bc7bf67ff54b8af74b2dd8
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2022-08-08 11:56:15 -0400

    updates

--- rmdb/new-schema.sql
+++
@@ -1,3 +0,0 @@
-# - Create two tables from NameTable
-CREATE TABLE NameTable (NameID INTEGER PRIMARY KEY, OwnerID INTEGER, Surname TEXT COLLATE ?????, Given TEXT COLLATE ?????, NameType INTEGER, , IsPrimary INTEGER, 
-Create Table BirthDeath (PersonID foreign key reference PersonTable.PersonID, BirthYear INTEGER, DeathYear INTEGER);
