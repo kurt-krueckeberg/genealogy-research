1f7179aaa4930304c1f7ea791c4d05ea0fb5fbfc
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2022-10-01 20:37:33 -0400

    updates

--- immanuel-decatur/db.sql
+++ immanuel-decatur/db.sql
@@ -1,24 +1,30 @@
 create table if not exits person (
    id int auto_increment,
+   sex ENUM('m', 'f'), 
    fname varchar(45) not null,
    lname varchar(20) not null,
-   birthdate date,
-   birthdorf varchar(15),
-   birthkreis varchar(20),
-   birthland varchar(25),
+   bdate date,
+#-- birth date, dord, kreis and land
+   bdorf varchar(15),
+   bkreis varchar(20),
+   bland varchar(25),
    is_founder boolean,
    primary key(id)
 );
 
 #--Church ceremonies performed on the date, by the pastorid at the parish.
+#--Marriage is a joint ceremony that involves two people. We can only view it as a ceremony that applies to
+#--two separate inviduals by using a composite key: (id, person)
 create table if not exists ceremony (
-  id int auto_increment,
+  id int,
+  person int,
   date date,
   type ENUM('baptize','confirm','marry'),
   parish varchar(20), 
   pastorid int,  
+  foreign key (person) reference person(id),
   foreign key (pastorid) reference person(id),
-  primary key(id)
+  primary key(id, person)
 );
 
 #--The married couple or 'family` consists of the husband husbid and wife wifeid. Their children, if any, are in the children table.
@@ -27,8 +33,6 @@ create table if not exists family (
   id int auto_increment,
   husbid int,
   wifeid int,
-#-- Is weddingid, which refers to the wedding ceremony of the couple, needed? 
-  weddingid int,
 #-- Emgiration is not a church ceremony, but here a family event (all the amily emigrated or most). But
 #-- this database is only for capturing the details about the Immanuel founders and their families. It is not an all-purpose
 #-- design for German couplesE and their families who emigrated--although it could be futher generalized.
@@ -36,7 +40,6 @@ create table if not exists family (
   unique key (husbid, wifeid),
   foreign key (husbid) references person(id), 
   foreign key (wifeid)  references  person(id), 
-  foreign key (weddingid) reference ceremony(id),
   primary key(id)
 );
 
