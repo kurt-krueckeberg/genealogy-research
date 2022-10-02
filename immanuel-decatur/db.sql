860c4c5625d5addb8202cd188d5a4c707a97cf88
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2022-10-02 07:56:18 -0400

    updates

--- immanuel-decatur/db.sql
+++ immanuel-decatur/db.sql
@@ -12,23 +12,40 @@ create table if not exits person (
    primary key(id)
 );
 
-#--Church ceremonies performed on the date, by the pastorid at the parish.
-#--Marriage is a joint ceremony that involves two people. We can only view it as a ceremony that applies to
-#--two separate inviduals by using a composite key: (id, person)
+#--This tracks Church ceremonies performed on the date, by the pastorid,
+#--at the parish.
+#--MSince mrriage is a joint ceremony that involves two people, we have 
+#--we create another table participants to associated, to linbk, the partipants
+R--for whom the ceremony occurs. Otherwise, the other attributes--date,
+#--pastor and parish--would be duplicated for each couple in a marriage.
 create table if not exists ceremony (
-  id int,
-  person int,
+  id int auto_increment,
   date date,
   type ENUM('baptize','confirm','marry'),
   parish varchar(20), 
   pastorid int,  
   foreign key (person) reference person(id),
   foreign key (pastorid) reference person(id),
-  primary key(id, person)
+  primary key(id)
 );
 
-#--The married couple or 'family` consists of the husband husbid and wife wifeid. Their children, if any, are in the children table.
-#--whose familyid references family(id) its husbid and wifeid who are the father and mother, respectively, of the child.
+#--This table associates particpants in a  ceremony with the ceremony
+#--and gives their role in the ceremony. It is a 'link' table whose key
+#--are the two attributes that that reference 1. the person in the ceremony
+#--and the ceremony in which they participated.
+create if not exists participants (
+  id int auto_increment,
+  cermid int,
+  role ENUM('baby', 'communicant', 'engaged', 'godparent'),
+  unique key(id, cermid),
+  foreign key (dermid) references ceremony(id),
+  foregin key (id) references person(id)
+};
+
+#--The married couple or 'family` consists of the husband husbid and wife
+#--wifeid. Their children, if any, are in the children table, whose familyid
+#--references family(id) its husbid and wifeid who are the father and mother,
+#--respectively, of the child.
 create table if not exists family (
   id int auto_increment,
   husbid int,
@@ -43,14 +60,15 @@ create table if not exists family (
   primary key(id)
 );
 
+#--The godprents table's role is performed by the participants table.
 #--List of Pathen or Taufpathen at baptism
-create table if not exist godparent (
-  id int auto_increment,
-  baptismid int,
-  foreign key (baptismid) references ceremony(id),
-  foreign key (id) references person(id),
-  primary key(id)
-);
+#--create table if not exist godparent (
+#--  id int auto_increment,
+#--  baptismid int,
+#--  foreign key (baptismid) references ceremony(id),
+#--  foreign key (id) references person(id),
+#--  primary key(id)
+#--);
 
 create table if not exists children (
   id int auto_increment,
