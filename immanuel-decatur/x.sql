ab855009518c49f335deea80d52022a4dd0184c8
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2022-10-02 10:37:59 -0400

    updates

--- immanuel-decatur/x.sql
+++
@@ -1,7 +0,0 @@
-#--The specific problem with German umlauts is often described as 'dictionary order' or 'phone-book order'. 
-#--The Unicode default is okay for the former but if you want 'Ü' = 'UE', then you could consider 
-#--utf8mb4_de_pb_0900_ai_ci or utf8mb4_german2_ci, assuming character set is utf8mb4.
-CREATE DATABASE if not exists imman CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_german2_ci';
-
-#--Create user and grant privileges to the database:
-#--GRANT ALL PRIVILEGES ON imman.* TO 'kurt'@'localhost' identified by 'kk0457';
