b1f0286720bff7ff700b8e7e4dba3e18835e74ca
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2022-10-04 09:03:47 -0400

    updates

--- immanuel-decatur/insert-code.php
+++ immanuel-decatur/insert-code.php
@@ -9,18 +9,32 @@ $options = [
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Make the default fetch be an associative array
 ];
 
+class pdoInserer {
+
+   public function __construct(\PDP $pdo, string $sql) {}
+  
+}
+
+function insert(\PDOStatement $s)
+{
+
+}
+
 try {
 
-  $pdo = new \PDO($dsn, "kurt", "kk0457", $options);
+ $pdo = new \PDO($dsn, "kurt", "kk0457", $options);
 
-  echo "pdo successfully created.\n";
 
-$sql = "INSERT INTO person (fname, lname, sex, fid, mid, bdate, bdorf, bkreis, bland, founder) VALUES (:fname, :lname, :sex, :fid, :mid, :bdate, :bdorf, :bkreis, :bland, :founder)";
+ $sql = "INSERT INTO person (fname, lname, sex, fid, mid, bdate, bdorf, bkreis, bland, founder) VALUES (:fname, :lname, :sex, :fid, :mid, :bdate, :bdorf, :bkreis, :bland, :founder)";
+ // $sql = "INSERT INTO person (fname, lname, sex, fid, mid, bdate, bdorf, bkreis, bland, founder) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-//Prepare the statement.
-$stmt = $pdo->prepare($sql);
+ //Prepare the statement.
+ $stmt = $pdo->prepare($sql);
 
-$input = [
+/*
+ Read and parse file and build this array:
+ 
+ $input = [
  'fname' => 'first',
  'lname' =>  'last',
  'sex' =>  'm',
@@ -31,10 +45,14 @@ $input = [
  'bkreis' =>  ' ',
  'bland' =>  ' ',
  'founder' => true,
-    ];
+ ];
 
-$rc = $stmt->execute($input);
+*/
 
+ $rc = $stmt->execute($input);
+
+ $id = $pdo->lastInsertId();
+ // Get last inserted id!
 /*
  * Insert all the persons listed in the biographies.
  * Then insert ceremonies, families and children.
